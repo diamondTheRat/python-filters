0d819dd36528d559567bc1e9d4abd0d67fd3b1f3
dia <124006582+diamondTheRat@users.noreply.github.com>
2024-10-10 23:07:44 +0300

    i did a dumb shit before

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
-from custom_libraries.filters.ascii import ascii
-from custom_libraries.filters.kuwahara import kuwahara
-from custom_libraries.filters.floyd_dithering import floyd_dithering, SECONDARY_DITHER_MATRIX
-from custom_libraries.filters.binary_overlay import binary_overlay
-from custom_libraries.filters.blur import gaussian_blur, fast_blur
-from custom_libraries.filters.pixelate import pixelate, pixel_art, Palettes
+from filters.ascii import ascii
+from filters.kuwahara import kuwahara
+from filters.floyd_dithering import floyd_dithering, SECONDARY_DITHER_MATRIX
+from filters.binary_overlay import binary_overlay
+from filters.blur import gaussian_blur, fast_blur
+from filters.pixelate import pixelate, pixel_art, Palettes
 # import warnings
